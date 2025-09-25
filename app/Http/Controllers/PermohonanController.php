4d1ebf656c4c937afe234adb3bf6adb552fb1ce6
MUlilAMrI23091397091 <149356238+MUlilAMrI23091397091@users.noreply.github.com>
2025-09-25 08:23:05 +0700

    fix(permohonan): remove pagination limit to show all 30 data in table

--- app/Http/Controllers/PermohonanController.php
+++ app/Http/Controllers/PermohonanController.php
@@ -102,7 +102,7 @@ class PermohonanController extends Controller
             });
         }
 
-        $permohonans = $permohonans->orderBy('created_at', 'desc')->paginate(10);
+        $permohonans = $permohonans->orderBy('created_at', 'desc')->get();
         
         // Ambil daftar sektor unik dari database dan gabungkan dengan sektor yang tersedia
         $sektorsFromDb = Permohonan::select('sektor')->whereNotNull('sektor')->distinct()->pluck('sektor');
